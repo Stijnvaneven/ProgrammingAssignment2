100aa269985a69ed2b0396eca0b3f7fa0b64a289
Stijnvaneven <Stijn.vaneven@nuance.com>
2015-02-21 11:54:16 -0500

    Initial modifications to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,36 @@
 
 ## Write a short comment describing this function
 
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
 makeCacheMatrix <- function(x = matrix()) {
   ## SVE: Let's Try
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
 }
 
@@ -14,5 +42,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## SVE: Let's Try
   
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
