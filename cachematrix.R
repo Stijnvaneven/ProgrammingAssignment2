ff02a209e40453028be372adab583d7d54137604
Stijnvaneven <Stijn.vaneven@nuance.com>
2015-02-21 11:24:30 -0500

    Initial edit of cachematrix.R
    This is the first commit to run through the process

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## SVE: Let's Try
 
 }
 
@@ -11,5 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  ## SVE: Let's Try
+  
         ## Return a matrix that is the inverse of 'x'
 }
